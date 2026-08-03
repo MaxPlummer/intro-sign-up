195a801010f941e5147e77947e227351742cb137
MaxPlummer <maxwellvplummer@gmail.com>
2026-08-03 14:26:09 -0400

    Edited html form, edited js to get form values.

--- validate.js
+++ validate.js
@@ -1,15 +1,38 @@
 // Credit to https://www.geeksforgeeks.org/javascript/how-to-validate-email-address-using-regexp-in-javascript/#google_vignette
+function validate() {
+  event.preventDefault();
+  // A liberal regex for a variety of first/last names
+  const nameRegex = /^[a-zA-Z-']+/;
+  // A basic regex for email validation
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  // Password regex with 8-character minimum
+  const pwdRegex =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
+  const userFirst = document.forms["registerForm"]["fname"].value;
+  const userLast = document.forms["registerForm"]["lname"].value;
+  const userEmail = document.forms["registerForm"]["email"].value;
+  const userPwrd = document.forms["registerForm"]["password"].value;
 
-// A liberal regex for a variety of first/last names
-let nameRegex = /^[a-zA-Z-'\s]+/;
-// A basic regex for email validation
-let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-// Password regex with 8-character minimum
-let pwdRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
-let temp = "max@gmail.com";
+  if (nameRegex.test(userFirst)) {
+    console.log("Valid First Name");
+  } else {
+    console.log("Invalid First Name");
+  }
+  if (nameRegex.test(userLast)) {
+    console.log("Valid Last Name");
+  } else {
+    console.log("Invalid Last Name");
+  }
+  if (nameRegex.test(userEmail)) {
+    console.log("Valid Email");
+  } else {
+    console.log("Invalid Email");
+  }
+  if (nameRegex.test(userPwrd)) {
+    console.log("Valid Password");
+  } else {
+    console.log("Invalid Password");
+  }
 
-if (emailRegex.test(temp)) {
-    console.log("Valid");
-} else {
-    console.log("Invalid");
+  console.log(userFirst, userLast, userEmail, userPwrd);
 }
